ccea4c0f0087de90d93a9e6a56c3cac939551dd3
Alexander Miller <alles.mil@gmail.com>
2013-05-03 16:26:58 +0200

    split of channels (fixes #1) and generation of bit planes (fixes #3)

--- frames2jb2.sh
+++ frames2jb2.sh
@@ -10,5 +10,32 @@
 
 for f in out/*.png
 do
-	jbig2 -s $f > $f.jb2
+	echo "frame $f"
+	convert $f -channel R -separate out/temp.png
+	# MSB
+	convert out/temp.png -fx '(int(u*255)&128)' out/tempbp.png
+	jbig2 -s out/tempbp.png > $f.7.jb2
+
+	convert out/temp.png -fx '(int(u*255)&64)' out/tempbp.png
+	jbig2 -s out/tempbp.png > $f.6.jb2
+
+	convert out/temp.png -fx '(int(u*255)&32)' out/tempbp.png
+	jbig2 -s out/tempbp.png > $f.5.jb2
+
+	convert out/temp.png -fx '(int(u*255)&16)' out/tempbp.png
+	jbig2 -s out/tempbp.png > $f.4.jb2
+
+	convert out/temp.png -fx '(int(u*255)&8)' out/tempbp.png
+	jbig2 -s out/tempbp.png > $f.3.jb2
+
+	convert out/temp.png -fx '(int(u*255)&4)' out/tempbp.png
+	jbig2 -s out/tempbp.png > $f.2.jb2
+
+	convert out/temp.png -fx '(int(u*255)&2)' out/tempbp.png
+	jbig2 -s out/tempbp.png > $f.1.jb2
+
+	convert out/temp.png -fx '(int(u*255)&1)' out/tempbp.png
+	jbig2 -s out/tempbp.png > $f.0.jb2
+
+	rm out/temp.png
 done
