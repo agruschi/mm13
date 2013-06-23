d6ca983d9b9011bfadcde193bbd20953033be43b
Alexander Miller <alles.mil@gmail.com>
2013-06-23 15:18:09 +0200

    frames2jb2.sh now uses RGB channels, lossless

--- sh/frames2jb2.sh
+++ sh/frames2jb2.sh
@@ -10,32 +10,96 @@
 
 for f in out/*.png
 do
-	echo "frame $f"
+	# ---------- Red channel --------------------------------------------------
+
 	convert $f -channel R -separate out/temp.png
+	
 	# MSB
 	convert out/temp.png -fx '(int(u*255)&128)' out/tempbp.png
-	jbig2 -s out/tempbp.png > $f.7.jb2
+	jbig2 out/tempbp.png > $f.r.7.jb2
 
 	convert out/temp.png -fx '(int(u*255)&64)' out/tempbp.png
-	jbig2 -s out/tempbp.png > $f.6.jb2
+	jbig2 out/tempbp.png > $f.r.6.jb2
 
 	convert out/temp.png -fx '(int(u*255)&32)' out/tempbp.png
-	jbig2 -s out/tempbp.png > $f.5.jb2
+	jbig2 out/tempbp.png > $f.r.5.jb2
 
 	convert out/temp.png -fx '(int(u*255)&16)' out/tempbp.png
-	jbig2 -s out/tempbp.png > $f.4.jb2
+	jbig2 out/tempbp.png > $f.r.4.jb2
 
 	convert out/temp.png -fx '(int(u*255)&8)' out/tempbp.png
-	jbig2 -s out/tempbp.png > $f.3.jb2
+	jbig2 out/tempbp.png > $f.r.3.jb2
 
 	convert out/temp.png -fx '(int(u*255)&4)' out/tempbp.png
-	jbig2 -s out/tempbp.png > $f.2.jb2
+	jbig2 out/tempbp.png > $f.r.2.jb2
 
 	convert out/temp.png -fx '(int(u*255)&2)' out/tempbp.png
-	jbig2 -s out/tempbp.png > $f.1.jb2
+	jbig2 out/tempbp.png > $f.r.1.jb2
 
 	convert out/temp.png -fx '(int(u*255)&1)' out/tempbp.png
-	jbig2 -s out/tempbp.png > $f.0.jb2
+	jbig2 out/tempbp.png > $f.r.0.jb2
+
+	rm out/temp.png
+
+	# ---------- Green channel --------------------------------------------------
+
+	convert $f -channel G -separate out/temp.png
+	
+	# MSB
+	convert out/temp.png -fx '(int(u*255)&128)' out/tempbp.png
+	jbig2 out/tempbp.png > $f.g.7.jb2
+
+	convert out/temp.png -fx '(int(u*255)&64)' out/tempbp.png
+	jbig2 out/tempbp.png > $f.g.6.jb2
+
+	convert out/temp.png -fx '(int(u*255)&32)' out/tempbp.png
+	jbig2 out/tempbp.png > $f.g.5.jb2
+
+	convert out/temp.png -fx '(int(u*255)&16)' out/tempbp.png
+	jbig2 out/tempbp.png > $f.g.4.jb2
+
+	convert out/temp.png -fx '(int(u*255)&8)' out/tempbp.png
+	jbig2 out/tempbp.png > $f.g.3.jb2
+
+	convert out/temp.png -fx '(int(u*255)&4)' out/tempbp.png
+	jbig2 out/tempbp.png > $f.g.2.jb2
+
+	convert out/temp.png -fx '(int(u*255)&2)' out/tempbp.png
+	jbig2 out/tempbp.png > $f.g.1.jb2
+
+	convert out/temp.png -fx '(int(u*255)&1)' out/tempbp.png
+	jbig2 out/tempbp.png > $f.g.0.jb2
+
+	rm out/temp.png
+
+	# ---------- Blue channel --------------------------------------------------
+
+	convert $f -channel B -separate out/temp.png
+	
+	# MSB
+	convert out/temp.png -fx '(int(u*255)&128)' out/tempbp.png
+	jbig2 out/tempbp.png > $f.b.7.jb2
+
+	convert out/temp.png -fx '(int(u*255)&64)' out/tempbp.png
+	jbig2 out/tempbp.png > $f.b.6.jb2
+
+	convert out/temp.png -fx '(int(u*255)&32)' out/tempbp.png
+	jbig2 out/tempbp.png > $f.b.5.jb2
+
+	convert out/temp.png -fx '(int(u*255)&16)' out/tempbp.png
+	jbig2 out/tempbp.png > $f.b.4.jb2
+
+	convert out/temp.png -fx '(int(u*255)&8)' out/tempbp.png
+	jbig2 out/tempbp.png > $f.b.3.jb2
+
+	convert out/temp.png -fx '(int(u*255)&4)' out/tempbp.png
+	jbig2 out/tempbp.png > $f.b.2.jb2
+
+	convert out/temp.png -fx '(int(u*255)&2)' out/tempbp.png
+	jbig2 out/tempbp.png > $f.b.1.jb2
+
+	convert out/temp.png -fx '(int(u*255)&1)' out/tempbp.png
+	jbig2 out/tempbp.png > $f.b.0.jb2
 
 	rm out/temp.png
 done
